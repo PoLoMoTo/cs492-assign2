69fdf5be35f96afff35810cf3dee61ecc2f8ed8c
PoLoMoTo <cbarnwel@stevens.edu>
2018-04-14 23:09:43 -0400

    I think I got paging working?

--- main.cpp
+++ main.cpp
@@ -28,7 +28,15 @@ int main(int argc, char const *argv[]){
     string ptraceFile = argv[2];
     int pageSize = stoi(argv[3]);
     string algorithm = argv[4];
-    string paging = argv[5]; // + = Pre-paging     - = Demand paging
+    int paging; // + = Pre-paging     - = Demand paging
+    if (argv[5][0] == '+'){
+        paging = 1;
+    } else if (argv[5][0] == '-'){
+        paging = 0;
+    } else {
+        cout << "Ur drunk go home m8" << endl;
+        return 1;
+    }
     string line;
     int mainPages = 512/pageSize;
     int mainMemory[mainPages];
@@ -137,6 +145,88 @@ int main(int argc, char const *argv[]){
                 }
             }
             pageTables.at(pid).at(location/pageSize)[2] = counter;
+
+            // If pre-paging is on find the next page and do it all over again
+            if (paging == 1){
+                int nextPid = pid; // Might not actually be different but it could be so yea...
+                int nextPage = (location/pageSize)+1;
+                if (nextPage >= pageTables.at(nextPid).size()){
+                    nextPid++;
+                    nextPage = 0;
+                    if (nextPid >= pageTables.size())
+                        nextPid = 0;
+                }
+                while (pageTables.at(nextPid).at(nextPage)[1]){
+                    nextPage++;
+                    if (nextPage >= pageTables.at(nextPid).size()){
+                        nextPid++;
+                        nextPage = 0;
+                        if (nextPid >= pageTables.size())
+                            nextPid = 0;
+                    }
+                }
+                //--------------------------------------------------------
+
+                if (mainMemory[mainPages-1] == -1){
+                    for (int i = 0; i < mainPages; i++){
+                        if (mainMemory[i] == -1){
+                            pageTables.at(nextPid).at(nextPage)[1] = 1;
+                            mainMemory[i] = pageTables.at(nextPid).at(nextPage)[0];
+                            Rbit[i] = 1;
+                            break;
+                        }
+                    }
+                } else if (algorithm == "FIFO"){
+                    for (int i = 0; i < totalPrograms; i++){
+                        if (pageTables.at(i).at(0)[0] <= mainMemory[firstIn] && mainMemory[firstIn] <= pageTables.at(i).at(pageTables.at(i).size() - 1)[0]){
+                            pageTables.at(i).at(mainMemory[firstIn] - pageTables.at(i).at(0)[0])[1] = 0;
+                            break;
+                        }
+                    }
+                    mainMemory[firstIn] = pageTables.at(nextPid).at(nextPage)[0];
+                    pageTables.at(nextPid).at(nextPage)[1] = 1;
+                    firstIn = (mainPages-1) ? 0 : firstIn + 1;
+                } else if (algorithm == "LRU"){
+                    unsigned long lowest = 0;
+                    int nextPidpage[3]; // nextPid, Program Page, Main Page
+                    for (int i = 0; i < mainPages && mainMemory[i] != -1; i++){
+                        for (int j = 0; j < totalPrograms; j++){
+                            // cout << pageTables.at(j).at(0)[0] << ',' << mainMemory[i] << ',' << pageTables.at(j).at(pageTables.at(j).size() - 1)[0] << endl;
+                            if (pageTables.at(j).at(0)[0] <= mainMemory[i] && mainMemory[i] <= pageTables.at(j).at(pageTables.at(j).size() - 1)[0]){
+                                if (i == 0 || pageTables.at(j).at(mainMemory[i] - pageTables.at(j).at(0)[0])[2] < lowest){
+                                    lowest = pageTables.at(j).at(mainMemory[i] - pageTables.at(j).at(0)[0])[2];
+                                    nextPidpage[0] = j;
+                                    nextPidpage[1] = mainMemory[i] - pageTables.at(j).at(0)[0];
+                                    nextPidpage[2] = i;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                    pageTables.at(nextPidpage[0]).at(nextPidpage[1])[1] = 0;
+                    mainMemory[nextPidpage[2]] = pageTables.at(nextPid).at(nextPage)[0];
+                    pageTables.at(nextPid).at(nextPage)[1] = 1;
+                } else if (algorithm == "Clock"){
+                    while (Rbit[firstIn] != 0){
+                        Rbit[firstIn] = 0;
+                        firstIn++;
+                    }
+                    Rbit[firstIn] = 1;
+                    for (int i = 0; i < totalPrograms; i++){
+                        if (pageTables.at(i).at(0)[0] <= mainMemory[firstIn] && mainMemory[firstIn] <= pageTables.at(i).at(pageTables.at(i).size() - 1)[0]){
+                            pageTables.at(i).at(mainMemory[firstIn] - pageTables.at(i).at(0)[0])[1] = 0;
+                            break;
+                        }
+                    }
+                    mainMemory[firstIn] = pageTables.at(nextPid).at(nextPage)[0];
+                    pageTables.at(nextPid).at(nextPage)[1] = 1;
+                    firstIn = (mainPages-1) ? 0 : firstIn + 1;
+                }
+                pageTables.at(nextPid).at(nextPage)[2] = counter;
+
+                //--------------------------------------------------------
+            }
+
             counter++;
         }
         ptrace.close();
